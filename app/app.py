c3896c1a1ef2dac6ac9de57e5ae6e66f3eb9e8f5
mwcodebase <mwaldron.personal@gmail.com>
2021-02-18 09:54:11 -0500

    rough draft complete

--- app/app.py
+++ app/app.py
@@ -1,36 +1,59 @@
+from argparse import ArgumentParser
 from selenium.webdriver import Firefox
-from selenium.webdriver.firefox.options import Options as web_options
-from os import path, getcwd
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.support.ui import Select
 
-def get_credentials():
-  # Retrieves the absolute path to the credentials file regardless of where the script is being run from
-  creds_path = getcwd() + "/" + path.dirname(__file__) + "credentials.env"
-
-  with open(creds_path, "r") as env_file:
-    return env_file.read().strip().split()
+from helper import get_credentials, get_name, get_day_name
 
 if __name__ == "__main__":
+  # Create optional command line arguments
+  cli_parser = ArgumentParser()
+  cli_parser.add_argument("--manual", "-m", help="Run and chose time manually", action="store_true")
+  cli_args = cli_parser.parse_args()
+
+  # Do the thing
   try:
+    opts = Options()
+    opts.headless = True
+    driver = Firefox(options=opts)
+
     # Direct link to the amenities site
-    url = "https://www.avalonaccess.com/Information/Information/Amenities"
-    
-    web_opts = web_options()
-    web_opts.set_headless()
-    browser = Firefox(options=web_opts)
-    
-    browser.get(url)
+    driver.get("https://www.avalonaccess.com/Information/Information/Amenities")
 
     # login
-    username, password = get_credentials()
+    username, password = get_credentials(cli_args.manual)
     
-    browser.find_element_by_id("UserName").send_keys(username)
-    browser.find_element_by_id("password").send_keys(password)
-    browser.find_element_by_id("submit-sign-in").click()
+    driver.find_element_by_id("UserName").send_keys(username)
+    driver.find_element_by_id("password").send_keys(password)
+    driver.find_element_by_id("submit-sign-in").click()
 
     # enter reservation screen
-    browser.find_element_by_id("reserve").click()
+    driver.find_element_by_id("reserve").click()
 
-    print(browser.page_source.encode("utf-8"))
+    ### Fill out form ###
+    # Note: script assumes reservations are being made for the current day
+    # There is a calendar selector, but it defaults to the current day
+
+    # Reservation time - the time values on the drop down are appended by the current day
+    if cli_args.manual:
+      print("Work in progress!")
+    else:
+      # Default behavior is to reserve 4:00pm for the current day
+      day = get_day_name()
+      selection = Select(driver.find_element_by_id("SelStartTime"))
+      selection.select_by_value(f"{day}-4:00 PM-5:00 PM")
+
+    # Number of people
+    driver.find_element_by_id("NumberOfPeople").clear()
+    driver.find_element_by_id("NumberOfPeople").send_keys("1")
+
+    # Names of people on reservation
+    name = get_name(cli_args.manual)
+    driver.find_element_by_id("ReservationNames").send_keys(name)
+
+    # Agree to TOS & submit!
+    driver.find_element_by_id("reservation-terms").click()
+    driver.find_element_by_id("submit-new-reservation").click()
 
   finally:
-    browser.close()
+    driver.close()
